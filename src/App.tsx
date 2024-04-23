9739af4798199effbf4d341b1379b5ec68ad18ee
balagurova <anastasi.balagurova@gmail.com>
2024-04-23 19:21:54 +0300

    Load data

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,42 @@
-import undpLogo from './assets/undp-logo-blue.svg';
+import { useEffect, useState } from 'react';
+import { csv } from 'd3-fetch';
+import { ChoroplethMap } from './Components/Graphs/Maps/ChoroplethMap';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    csv(
+      `https://raw.githubusercontent.com/UNDP-Data/dv-sustainable-finance-hub-data-repo/main/SFH_data.csv`,
+    )
+      .then((loadedData: any) => {
+        setData(loadedData);
+      })
+      .catch((err: any) => {
+        console.error('Error loading the CSV file:', err);
+      });
+  }, []);
+  console.log(data);
   return (
     <div className='undp-container flex-div flex-wrap flex-hor-align-center margin-top-13 margin-bottom-13'>
-      <div>
-        <img
-          src={undpLogo}
-          className='logo react'
-          alt='React logo'
-          width='72px'
-          style={{ margin: 'auto' }}
-        />
-      </div>
-      <h3
-        className='undp-typography'
-        style={{ textAlign: 'center', width: '100%' }}
-      >
-        This is the starter kit for data visualization utilities like bar
-        charts, line chart, area charts etc. You can read the documentation{' '}
-        <a
-          href='mailto:data@undp.org'
-          target='_blank'
-          rel='noreferrer'
-          className='undp-style'
-        >
-          here
-        </a>
-        .
-        <br />
-        <br />
-        Contact us at{' '}
-        <a
-          href='mailto:data@undp.org'
-          target='_blank'
-          rel='noreferrer'
-          className='undp-style'
-        >
-          data@undp.org
-        </a>{' '}
-        if you have any feedback or questions.
-      </h3>
+      <ChoroplethMap
+        data={[
+          { countryCode: 'IND', x: 1 },
+          { countryCode: 'FIN', x: 2 },
+          { countryCode: 'IDN', x: 3 },
+          { countryCode: 'ZAF', x: 4 },
+          { countryCode: 'PER', x: 5 },
+          { countryCode: 'PAK', x: 6 },
+          { countryCode: 'USA', x: 7 },
+          { countryCode: 'SWE', x: 8 },
+        ]}
+        graphTitle='Title of the graph'
+        width={1200}
+        height={900}
+        source='Organization ABC'
+        sourceLink='www.example.com'
+        domain={[2, 4, 6, 8]}
+      />
     </div>
   );
 }
